99f249467773dd135155e505b543af9908919fcb
wkddnjswl7 <wkddnjswl7@naver.com>
2024-11-01 20:14:43 +0900

    feat : mui 컴포넌트 생성 후 소셜 로그인 및 마이페이지에 import

---
+++ src/components/MuiColor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+    palette: {
+        primary : {
+            main : "#2e8b57",
+            light : "#ACE1AF"
+        }
+    }
+})

54c2be04a3845b103991fd01a5249a0d7942277c
Unji <121096466+wkddnjswl7@users.noreply.github.com>
2024-11-01 20:42:41 +0900

    feat : mui 컴포넌트 생성 후 소셜 로그인 및 마이페이지에 import (#28)

---
+++ src/components/MuiButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+//배경색 없는 Button
+export const CustomButton1 = styled(Button)(({ theme }) => ({
+    color: '#2e8b57',
+    '&:hover': {
+        backgroundColor: 'lightgray',
+    },
+}));
+
+export const CustomButton2 = styled(Button)(({ theme }) => ({
+    backgroundColor: theme.palette.primary.main,
+    color: 'white',
+    fontSize:'bold',
+    '&:hover': {
+        backgroundColor: theme.palette.primary.light,
+    },
+}));

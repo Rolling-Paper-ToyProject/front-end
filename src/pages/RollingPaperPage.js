d21d78bf607423d730728c1706517b74b219f9e0
Unji <121096466+wkddnjswl7@users.noreply.github.com>
2024-11-16 19:35:40 +0900

    [main] Paper 각각의 롤링페이퍼 하트모양으로 수정 및 롤링페이퍼에 있는 Modal 내 내용 및 버튼 CSS 수정 (#46)
    * main : 롤링페이퍼 모양 설정 및 롤링페이퍼 Modal Css 수정
    
    * main : 불필요한 내용 제거 및 각 CSS 별 설명 보충

--- src/pages/RollingPaperPage.js
+++ src/pages/RollingPaperPage.js
@@ -1,87 +1,86 @@
 import React, { useEffect, useState } from "react";
 import PaperItem from "../components/PaperItem";
-import '../styles/pages/RollingPaperPage.css'
+import "../styles/pages/RollingPaperPage.css";
 import CreatePaperModal from "../components/CreatePaperModal";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { CustomLogout } from "../components/MuiButton";
 
 const RollingPaperPage = () => {
+  const location = useLocation();
+  const { rollId, rollName } = location.state || {};
+  const navigate = useNavigate();
+  const [papers, setPapers] = useState([]);
+  const [isCreatePaperModalOpen, setIsCreatePaperModalOpen] = useState(false);
+  const token = localStorage.getItem("Authorization");
 
-    const location = useLocation();
-    const { rollId, rollName } = location.state || {};
-    const navigate = useNavigate();
-    const [papers, setPapers] = useState([]);
-    const [isCreatePaperModalOpen, setIsCreatePaperModalOpen] = useState(false);
-    const token = localStorage.getItem("Authorization");
+  useEffect(() => {
+    const fetchPaperData = async () => {
+      if (!token) {
+        alert("로그인 상태가 아닙니다. 로그인 후 이용해주세요.");
+        navigate("/");
+        return;
+      }
 
-    useEffect(() => {
-        const fetchPaperData = async () => {
-            
-            if(!token) {
-                alert("로그인 상태가 아닙니다. 로그인 후 이용해주세요.")
-                navigate('/');
-                return;
-            }
+      try {
+        // 페이퍼 정보 가져오기
+        const paperResponse = await axios.get(
+          `http://localhost:8080/paper/rolls/${rollId}`,
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
 
-            try {
-                // 페이퍼 정보 가져오기
-                const paperResponse = await axios.get(`http://localhost:8080/paper/rolls/${rollId}`, {
-                    headers: {
-                        "Authorization": token
-                    }
-                });
+        const paperData = paperResponse.data;
+        console.log("Paper Data Response:", paperData);
+        setPapers(paperData.data || []); // 빈 배열 fallback 추가
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
 
-                const paperData = paperResponse.data;
-                console.log("Paper Data Response:", paperData);
-                setPapers(paperData.data || []); // 빈 배열 fallback 추가
+    fetchPaperData();
+  }, [rollId]);
 
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        }
+  // 모달을 여는 함수
+  const showCreateModal = () => {
+    setIsCreatePaperModalOpen(true);
+  };
 
-        fetchPaperData();
-    }, [rollId]);
+  // 모달을 닫는 함수
+  const closeModal = () => {
+    setIsCreatePaperModalOpen(false);
+  };
 
-    // 모달을 여는 함수
-    const showCreateModal = () => {
-        setIsCreatePaperModalOpen(true);
-    } 
+  return (
+    <div>
+      <div className="header">
+        <p className="className">{rollName}</p>
+        <CustomLogout
+          className="add-paper-button"
+          onClick={showCreateModal}
+          style={{ fontWeight: "bold", fontSize: "16px" }}
+        >
+          작성
+        </CustomLogout>
+      </div>
 
-    // 모달을 닫는 함수
-    const closeModal = () => {
-        setIsCreatePaperModalOpen(false);
-    }
-
-    return (
-        <div>
-            <div className="header">
-                <p className="className">{rollName}</p>
-                <button className="add-paper-button" onClick={showCreateModal}>페이퍼 작성</button>
-            </div>
-
-            <div className="paper-container">
-                {/* RollingPaperDetail 컴포넌트 */}
-                {Array.isArray(papers) && papers.length > 0 ? (
-                    papers.map((paper) => (
-                        <PaperItem 
-                            key={paper.paperId}
-                            paper={paper}
-                        />
-                    ))
-                ) : (
-                    <p>작성된 페이퍼가 없습니다</p>
-                )}
-            </div>
-            {/* CreatePaperModal 컴포넌트 */}
-            {isCreatePaperModalOpen && 
-                <CreatePaperModal 
-                    rollId={rollId}
-                    closeModal={closeModal}
-                />}
-        </div>
-    )
-    
-}
+      <div className="paper-container">
+        {/* RollingPaperDetail 컴포넌트 */}
+        {Array.isArray(papers) && papers.length > 0 ? (
+          papers.map((paper) => <PaperItem key={paper.paperId} paper={paper} />)
+        ) : (
+          <p>작성된 페이퍼가 없습니다</p>
+        )}
+      </div>
+      {/* CreatePaperModal 컴포넌트 */}
+      {isCreatePaperModalOpen && (
+        <CreatePaperModal rollId={rollId} closeModal={closeModal} />
+      )}
+    </div>
+  );
+};
 
 export default RollingPaperPage;

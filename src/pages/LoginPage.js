a1d5ebb362d97aebed23de2569353470dc984fa7
wkddnjswl7 <wkddnjswl7@naver.com>
2024-11-19 01:53:03 +0900

    style : 소셜 로그인 페이지 CSS 수정 1. 불필요한 파일 삭제 2. height : 450px 이하로 화면 깨지는 현상 수정 3. 로고 이미지에 있는 폰트와 비슷하게 폰트 적용 4. 로그인 버튼 간격 및 로그인 Hover 시  타원형으로 생기던 현상 수정(  타원형 -> 원형으로 수정)

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import SocialLogin from '../components/SocialLogin'
-import '../styles/pages/LoginPage.css';
 import '../styles/common/Logo.css';
 import { useNavigate } from "react-router-dom";
 
@@ -19,7 +18,7 @@ const LoginPage = () => {
                 <img src="\images\\logo\image_logo.png" className="logo" alt="Logo" />
             </div>
 
-            <h2>Login</h2>
+            <h2 className="login">Login</h2>
             {/* 소셜 로그인 컴포넌트. 로그인 성공 시 handleSocialLoginSuccess 실행 */}
             <SocialLogin onLoginSuccess={handleSocialLoginSuccess}/>
         </div>

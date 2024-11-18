373a02b6e6d7ac1bf2fc4371da9e8cf772bb6d71
Unji <121096466+wkddnjswl7@users.noreply.github.com>
2024-11-18 14:09:51 +0900

    main : 학생 로그인 화면 라벨 추가 및 입력칸 너비 조정 (#49)

--- src/components/StudentSignin.js
+++ src/components/StudentSignin.js
@@ -1,105 +1,111 @@
-import { useEffect, useState } from "react"
-import '../styles/components/StudentSignin.css' 
+import { useEffect, useState } from "react";
+import "../styles/components/StudentSignin.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const StudentSignin = ({ url }) => {
-    const navigate = useNavigate();
-    const [pinNumber, setPinNumber] = useState('');
-    const [classCode, setClassCode] = useState('');
-    const [studentName, setStudentName] = useState('');
-    const token = localStorage.getItem("Authorization");
+  const navigate = useNavigate();
+  const [pinNumber, setPinNumber] = useState("");
+  const [classCode, setClassCode] = useState("");
+  const [studentName, setStudentName] = useState("");
+  const token = localStorage.getItem("Authorization");
 
-    useEffect(() => {
-        const fetchTeacherData = async () => {
-            if(token) {
-                try {
-                    const userResponse = await axios.get('http://localhost:8080/roll/me', {
-                        headers: {
-                            "Authorization": token
-                        }
-                    });
-                    const userData = userResponse.data;
-                    const foundItem = userData.data.find(item => item.url === url);
-                    
-                    if (foundItem) {
-                        const { rollId, rollName } = foundItem;                
-                        navigate(`/roll/${url}/join`, { state: { rollId, rollName } });
-                    }
-                } catch (error) {
-
-                }
-            }
-        }
-
-        fetchTeacherData();
-
-    }, [token, url, navigate]);
-
-    const handleSignin = async (e) => {
-        e.preventDefault();
+  useEffect(() => {
+    const fetchTeacherData = async () => {
+      if (token) {
         try {
-            const response = await axios.post(`http://localhost:8080/roll/${url}/join`, 
-                {
-                    "name": studentName,
-                    "classCode": classCode,
-                    "pinNumber": pinNumber
-                }
-            );
-
-            if(!response === 200) {
-                throw new Error("응답을 불러올 수 없습니다.")
+          const userResponse = await axios.get(
+            "http://localhost:8080/roll/me",
+            {
+              headers: {
+                Authorization: token,
+              },
             }
+          );
+          const userData = userResponse.data;
+          const foundItem = userData.data.find((item) => item.url === url);
 
-            const studentData = response.data;
-            const studentToken = studentData.data.accessToken;
-            const refreshToken = studentData.data.refreshToken;
-            const rollId = studentData.data.rollId;
-            const rollName = studentData.data.rollName;
+          if (foundItem) {
+            const { rollId, rollName } = foundItem;
+            navigate(`/roll/${url}/join`, { state: { rollId, rollName } });
+          }
+        } catch (error) {}
+      }
+    };
 
-            if (studentToken && refreshToken) {
-                localStorage.setItem("Authorization", `Bearer ${studentToken}`);
-                localStorage.setItem("RefreshToken", refreshToken);
-                navigate(`/roll/${url}/join`, { state: { rollId, rollName }})
-            }
+    fetchTeacherData();
+  }, [token, url, navigate]);
 
-        } catch (error) {
-            console.log("토큰 fetch 실패: ", error);
-            alert("사용자 정보를 불러올 수 없습니다.");
+  const handleSignin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `http://localhost:8080/roll/${url}/join`,
+        {
+          name: studentName,
+          classCode: classCode,
+          pinNumber: pinNumber,
         }
-    }
+      );
 
-    return(
+      if (!response === 200) {
+        throw new Error("응답을 불러올 수 없습니다.");
+      }
+
+      const studentData = response.data;
+      const studentToken = studentData.data.accessToken;
+      const refreshToken = studentData.data.refreshToken;
+      const rollId = studentData.data.rollId;
+      const rollName = studentData.data.rollName;
+
+      if (studentToken && refreshToken) {
+        localStorage.setItem("Authorization", `Bearer ${studentToken}`);
+        localStorage.setItem("RefreshToken", refreshToken);
+        navigate(`/roll/${url}/join`, { state: { rollId, rollName } });
+      }
+    } catch (error) {
+      console.log("토큰 fetch 실패: ", error);
+      alert("입력한 학급코드를 확인해주세요");
+    }
+  };
+
+  return (
+    <div className="student-container">
+      <form onSubmit={handleSignin} className="student-login-area">
         <div>
-            <form onSubmit={handleSignin}>
-                <div>
-                    <input 
-                    type="text" 
-                    placeholder="학급코드를 입력해주세요" 
-                    value={classCode}
-                    onChange={(e) => setClassCode(e.target.value)}
-                    required />
-                </div>
-                <div>
-                    <input 
-                    type="text" 
-                    placeholder="이름을 입력해주세요" 
-                    value={studentName}
-                    onChange={(e) => setStudentName(e.target.value)}
-                    required />
-                </div>
-                <div>
-                    <input 
-                    type="password" 
-                    placeholder="비밀번호 4자리를 입력해주세요" 
-                    value={pinNumber}
-                    onChange={(e) => setPinNumber(e.target.value)}
-                    required />
-                </div>
-                <button type="submit" className="signin-button">롤링페이퍼 입장</button>
-            </form>
+          <label>학급코드 </label>
+          <input
+            type="text"
+            value={classCode}
+            onChange={(e) => setClassCode(e.target.value)}
+            required
+          />
         </div>
-    )
-}
+        <div>
+          <label style={{ letterSpacing: "10px" }}>이름 </label>
+          <input
+            type="text"
+            value={studentName}
+            onChange={(e) => setStudentName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>비밀번호 </label>
+          <input
+            type="password"
+            value={pinNumber}
+            onChange={(e) => setPinNumber(e.target.value)}
+            maxLength={"4"}
+            required
+          />
+        </div>
+        <button type="submit" className="signin-button">
+          롤링페이퍼 입장
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default StudentSignin;

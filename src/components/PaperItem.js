d26bca77497b1322dbaea69631d56c2e5ef89e15
wkddnjswl7 <wkddnjswl7@naver.com>
2024-11-21 13:47:02 +0900

    style : 롤링페이퍼 각도 랜덤으로 조절

--- src/components/PaperItem.js
+++ src/components/PaperItem.js
@@ -6,6 +6,12 @@ const PaperItem = ({ paper }) => {
   const { paperId, content, authorName, authorRole } = paper;
   const [isPaperDetailModalOpen, setIsPaperDetailModalOpen] = useState(false);
 
+  // 랜덤 각도 설정
+  const randomRotation = () => {
+    // -5도에서 5도 사이의 랜덤 각도
+    return Math.floor(Math.random() * 10) - 5;
+  };
+
   const showPaperDetailModal = (paper) => {
     setIsPaperDetailModalOpen(true);
   };
@@ -21,6 +27,7 @@ const PaperItem = ({ paper }) => {
           key={paperId}
           onClick={() => showPaperDetailModal(paper)}
           className="paper-box"
+          style={{ transform: `rotate(${randomRotation()}deg)` }} // 각도 적용
         >
           <p className="fromName">From. {authorName}</p>
           <p className="roll-content">{content}</p>

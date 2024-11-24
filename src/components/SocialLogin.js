102bd04f5a6fd5793917a8fadf4069607f9a21ed
Soo186 <rlagustn9797@gmail.com>
2024-11-24 16:09:03 +0900

    fix : SocialLogin.js

--- src/components/SocialLogin.js
+++ src/components/SocialLogin.js
@@ -22,7 +22,7 @@ const SocialLogin = () => {
 
         // Spring Security로 간소화 적용 후 코드
         // Spring Security가 제공하는 네이버 OAuth2 인증 엔드포인트로 리다이렉트
-        window.location.href = BASE_URL + '/oauth2/authorization/naver';
+        window.location.href = BASE_URL + '/api/oauth2/authorization/naver';
 
     }
     
@@ -39,7 +39,7 @@ const SocialLogin = () => {
 
         // Spring Security로 간소화 적용 후 코드
         // Spring Security가 제공하는 카카오 OAuth2 인증 엔드포인트로 리다이렉트
-        window.location.href = BASE_URL + '/oauth2/authorization/kakao';
+        window.location.href = BASE_URL + '/api/oauth2/authorization/kakao';
         
     }
     

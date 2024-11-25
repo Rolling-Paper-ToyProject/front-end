b9360adc647d3817bd55994d28377644099ba1eb
CHW_1207 <dilkusha27@gmail.com>
2024-11-25 20:15:36 +0900

    ui/ux: 학생일 경우 타 사용자 페이퍼 열람 시 하단 여백 추가

--- src/components/PaperDetailModal.js
+++ src/components/PaperDetailModal.js
@@ -133,7 +133,7 @@ const PaperDetailModal = ({ paper, role, currentStudentId, closeModal, onUpdateP
             )} 
           </>
         ) : (
-          <p className="paper-detail-content">{content}</p>
+          <p className="paper-detail-content" style={{ marginBottom: "15px" }}>{content}</p>
         )}
       </div>
     </div>
